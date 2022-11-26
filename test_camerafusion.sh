75a79be1f625704380a763498ddc207aefc13be0
XDMickeyYau <35620428+XDMickeyYau@users.noreply.github.com>
2022-11-26 17:18:40 -0500

    change test dir

--- test_camerafusion.sh
+++ test_camerafusion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Start to test the model...."
 
-dataroot="/Data/dataset/RealSR/RealSR_patches"
+dataroot="./"
 name="camerafusion_l1" # You can replace 'camerafusion_l1' with 'camerafusion_l1sw' when testing the model trained by SW loss.
 scale='2'
 data='cam'
@@ -12,5 +12,5 @@ iter="401"
 python test.py \
     --model refsr  --name $name  --dataset_name $data  --chop True  --full_res True  --predict True \
     --load_iter $iter    --save_imgs True  --calc_psnr False  --gpu_ids $device  --scale $scale  --dataroot $dataroot
-
+echo "metric"
 python metrics.py  --device $device --name $name --load_iter $iter  --cam $cam  --dataroot $dataroot
