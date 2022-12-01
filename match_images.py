e45fef339fd6e096c3ea5ada49547d756c5baaf6
XDMickeyYau <35620428+XDMickeyYau@users.noreply.github.com>
2022-12-01 18:35:11 -0500

    match_images become command line module

--- match_images.py
+++ match_images.py
@@ -2,13 +2,14 @@
 import cv2
 import os
 import itertools
+import argparse
 
 # %%
 def read_img(hr_dir,sr_dir,file,seq):
     name, ext = os.path.splitext(file)
     hr = cv2.imread(os.path.join(hr_dir, name+ext))
     sr = cv2.imread(os.path.join(sr_dir, name+seq+ext))
-    assert hr is not None and sr is not None
+    assert hr is not None and sr is not None, "No corresponding SR image for HR"
     return hr, sr
 
 # %%
@@ -42,21 +43,33 @@ def match_size_multiple(hr_dir,sr_dir,sr_out_dir,seqs=[""]):
     files = os.listdir(hr_dir)
     print("files",files)
     for file, seq in itertools.product(files,seqs):
-        sr_out = match_size_single(hr_dir,sr_dir,file,seq)
-        write_img(sr_out,sr_out_dir,file,seq)    
+        try:
+            sr_out = match_size_single(hr_dir,sr_dir,file,seq)
+            write_img(sr_out,sr_out_dir,file,seq)    
+        except Exception as e:
+            print("[ERROR]:", e)  
 
 # %% [markdown]
 # main
 
 # %%
-def __main__():
-    hr_dir = "HR_raw"
-    sr_dir = "HR_mismatch"
-    sr_out_dir = "HR_match"
-    seqs = [""]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Image matching')
+    parser.add_argument('--hr_dir', help='name',required=True)
+    parser.add_argument('--sr_dir', help='name',required=True)
+    parser.add_argument('--sr_out_dir', help='name',required=True)
+    parser.add_argument('--seqs', nargs='*', help='name',required=False, default=[''])
+    args = parser.parse_args()
+    print("args",args)
+    
+    hr_dir = args.hr_dir 
+    sr_dir = args.sr_dir 
+    sr_out_dir = args.sr_out_dir
+    seqs = args.seqs
     match_size_multiple(hr_dir,sr_dir,sr_out_dir,seqs)
+    print("FINISHED")
 
 # %%
-#__main__()
+#python3 match_images.py --hr_dir HR_raw --sr_dir HR_mismatch --sr_out_dir HR_match --seqs ""
 
 
